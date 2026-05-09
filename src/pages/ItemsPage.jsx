985fa6c882b87ed8c2c9641b6dc83858f4a74cda
Tomzy <fadareemmanue14@gmail.com>
2026-05-09 20:02:36 +0100

    Fix mobile card layout

--- src/pages/ItemsPage.jsx
+++ src/pages/ItemsPage.jsx
@@ -183,7 +183,7 @@ async function load() {
           </p>
         </div>
       ) : (
-        <div className="mt-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+       <div className="mt-8 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {items.map((item) => (
             <ItemCard key={item.id} item={item} />
           ))}

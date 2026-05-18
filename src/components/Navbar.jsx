713e6005a40017be282597cebb0d6ea938f1ff90
Tomzy <fadareemmanue14@gmail.com>
2026-05-18 18:23:55 +0100

    nav improve

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,6 +76,7 @@ export default function Navbar() {
                 </div>
                 <div className="absolute -bottom-8 right-0 hidden group-hover:block bg-dark text-white text-xs rounded-lg px-2 py-1 whitespace-nowrap z-50">
                   {user?.name || "User"}
+                  <div className="flex items-center gap-0.5 sm:gap-1 overflow-x-auto">
                 </div>
               </div>
             </div>
